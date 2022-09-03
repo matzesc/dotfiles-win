b4af7446e967cd8ac568301f9619964c25251aeb
Mathias Schmalisch <mathias.schmalisch@gmail.com>
2022-09-03 10:56:54 -0400

    added Doom Emacs config files

---
+++ .config/doom/custom.el
@@ -0,0 +1,60 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("071cb4e4867144e6288e8cfe3678949359fa88c2b46224ca573858771c6afc9f" default))
+ '(indent-tabs-mode nil)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(vhdl-basic-offset 4)
+ '(vhdl-clock-edge-condition 'function)
+ '(vhdl-company-name "CommScope Inc, Forest, VA, USA")
+ '(vhdl-compile-use-local-error-regexp t)
+ '(vhdl-compiler "ModelSim")
+ '(vhdl-conditions-in-parenthesis t)
+ '(vhdl-copyright-string
+   "-------------------------------------------------------------------------------
+-- Restricted - Highly Confidential
+--
+-- COPYRIGHT
+--
+-- Copyright (c) <year> <company>
+-- This software program is the proprietary copyright material of CommScope
+-- Inc. No Transfer or assignment of such copyright is intended to be effected
+-- by the permitted use by an authorized user under license notwithstanding the
+-- terms of any agreement entered into by an end user with an intermediary
+-- other than CommScope Inc.
+--
+-- No part of this software program may be reproduced, amended, varied,
+-- re-written or stored in a retrieval system in any form or by any means
+-- without the prior written permission of CommScope Inc.
+")
+ '(vhdl-electric-mode t)
+ '(vhdl-include-port-comments t)
+ '(vhdl-indent-tabs-mode nil)
+ '(vhdl-index-menu t)
+ '(vhdl-reset-active-high t)
+ '(vhdl-reset-kind 'query)
+ '(vhdl-source-file-menu t)
+ '(vhdl-speedbar-display-mode 'directory)
+ '(vhdl-speedbar-save-cache nil)
+ '(vhdl-standard '(8 nil))
+ '(vhdl-stutter-mode t)
+ '(vhdl-upper-case-attributes t)
+ '(vhdl-upper-case-enum-values t)
+ '(vhdl-upper-case-keywords t)
+ '(word-wrap t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+(put 'customize-save-customized 'disabled nil)
